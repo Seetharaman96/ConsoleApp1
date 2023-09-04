02ec55f6e72bd24ae7e5e49171ef5d5faa9ea738
Seetharaman96 <seetharam.ad96@gmail.com>
2023-09-05 00:15:07 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main1()
+        {
+            Console.WriteLine("Hello Universe");
+        }
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Main1();
+        }
+    }
+}
