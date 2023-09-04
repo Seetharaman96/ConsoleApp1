636893f5743db34bfd79cf8437e28ffa60d890e6
Seetharaman96 <seetharam.ad96@gmail.com>
2023-09-05 00:17:57 +0530

    upload

--- Program.cs
+++ Program.cs
@@ -4,9 +4,14 @@ namespace MyApp
 {
     internal class Program
     {
+        static void Main2()
+        {
+            Console.WriteLine("Hello Galaxy");
+        }
         static void Main1()
         {
             Console.WriteLine("Hello Universe");
+            Main2();
         }
         static void Main()
         {
