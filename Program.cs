c516d13f16d87c708c663c7a759a379ea591513a
Seetharaman96 <seetharam.ad96@gmail.com>
2023-09-05 00:40:17 +0530

    Upload

--- Program.cs
+++ Program.cs
@@ -15,7 +15,18 @@ namespace MyApp
         }
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please enter your first name");
+
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your last name");
+
+            string LastName = Console.ReadLine();
+
+            //Console.WriteLine("Hello {0}{1}", FirstName, LastName);
+
+            Console.WriteLine("Hello " + FirstName + LastName);
+
             Main1();
         }
     }
